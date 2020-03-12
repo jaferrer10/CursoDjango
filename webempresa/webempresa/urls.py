f6265bdc2e5044e221188a0ee769439102ea7c03
Julian Ferrer <jaferrer10@hotmail.com>
2020-03-12 12:46:04 -0300

    Finalizando webempresa

--- webempresa/webempresa/urls.py
+++ webempresa/webempresa/urls.py
@@ -14,26 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from core import views
-"""
-Inicio home/
-Historia about/
-Servicios services/
-Visítanos store/
-Contacto contact/
-Blog blog/
-Sample sample/
-"""
+from django.urls import path, include
+
 urlpatterns = [
-    # Paths del core
-    path('',views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('services', views.services, name='services'),
-    path('store', views.store, name='store'),
-    path('contact', views.contact, name='contact'),
-    path('blog',views.blog, name='blog'),
-    path('sample',views.sample, name='sample'),
+    # Path del core
+    path('', include('core.urls')),
 
     # Path del admin
     path('admin/', admin.site.urls),
