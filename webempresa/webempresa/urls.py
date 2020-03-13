e044ddf1ee818dfe96ce982bff099ed06709c3fe
Julian Ferrer <jaferrer10@hotmail.com>
2020-03-13 12:32:43 -0300

    Diseñando servicios de la web empresarial

--- webempresa/webempresa/urls.py
+++ webempresa/webempresa/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     # Path del core
@@ -23,3 +24,6 @@ urlpatterns = [
     # Path del admin
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
